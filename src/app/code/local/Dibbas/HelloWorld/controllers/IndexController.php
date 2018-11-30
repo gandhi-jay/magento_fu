70c235422420144df5970564a1c7b707fd38b230
Jay Gandhi <jkgandhi92@gmail.com>
2018-11-30 17:17:42 +0530

    Added my first custom module

---
+++ src/app/code/local/Dibbas/HelloWorld/controllers/IndexController.php
@@ -0,0 +1,7 @@
+<?php
+class Dibbas_HelloWorld_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction() {
+        echo "We're echoing just to show that this is what's called, normally you'd have some kind of redirect going on here";
+    }
+}
