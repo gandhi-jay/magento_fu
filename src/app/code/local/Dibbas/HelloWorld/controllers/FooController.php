8e3dd809d362959dfd4888e21975a91c5c63f412
Jay Gandhi <jkgandhi92@gmail.com>
2018-11-30 17:58:59 +0530

    Sesss

--- src/app/code/local/Dibbas/HelloWorld/controllers/FooController.php
+++ src/app/code/local/Dibbas/HelloWorld/controllers/FooController.php
@@ -3,6 +3,11 @@
 class Dibbas_HelloWorld_FooController extends Mage_Core_Controller_Front_Action
 {
     public function IndexAction() {
-        echo "I can do this all night.";
+        echo '<dl>';            
+        foreach($this->getRequest()->getParams() as $key=>$value) {
+            echo '<dt><strong>Param: </strong>'.$key.'</dt>';
+            echo '<dl><strong>Value: </strong>'.$value.'</dl>';
+        }
+        echo '</dl>';
     }
 }
